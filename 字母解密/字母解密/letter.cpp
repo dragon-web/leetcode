319079844b4061be9375820d7fda1cb6ad15dd21
dragon-web <1582943265@qq.com>
2020-04-24 14:35:32 +0800

    解密

--- 字母解密/字母解密/letter.cpp
+++ 字母解密/字母解密/letter.cpp
@@ -65,7 +65,7 @@ int minDistance(string word1, string word2)
 
 	for (int i = 1; i <= len1; ++i) {
 		for (int j = 1; j <= len2; ++j) {        
-			// F(i,j) = min { F(i-1,j）+1, F(i,j-1) +1, F(i-1,j-1) +                      //(w1[i]==w2[j]?0:1) }                 // 判断word1的第i个字符是否与word2的第j个字符相等                if (word1[i - 1] == word2[j - 1]) {                    f[i][j] = 1 + min(f[i][j - 1], f[i - 1][j]);                    // 字符相等，F(i-1,j-1)编辑距离不变                    f[i][j] = min(f[i][j], f[i - 1][j - 1]);                }                else {                    f[i][j] = 1 + min(f[i][j - 1], f[i - 1][j]);                    // 字符不相等，F(i-1,j-1)编辑距离 + 1                    f[i][j] = min(f[i][j], 1 + f[i - 1][j - 1]);                }            }        }
+// F(i,j) = min { F(i-1,j）+1, F(i,j-1) +1, F(i-1,j-1) + //(w1[i]==w2[j]?0:1) }                 // 判断word1的第i个字符是否与word2的第j个字符相等                if (word1[i - 1] == word2[j - 1]) {                    f[i][j] = 1 + min(f[i][j - 1], f[i - 1][j]);                    // 字符相等，F(i-1,j-1)编辑距离不变                    f[i][j] = min(f[i][j], f[i - 1][j - 1]);                }                else {                    f[i][j] = 1 + min(f[i][j - 1], f[i - 1][j]);                    // 字符不相等，F(i-1,j-1)编辑距离 + 1                    f[i][j] = min(f[i][j], 1 + f[i - 1][j - 1]);                }            }        }
 
 			return f[len1][len2];
 		}
