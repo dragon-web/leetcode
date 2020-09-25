05d0636044da1bc4568791d38feb1a481e223b08
dragon-web <1582943265@qq.com>
2020-09-25 16:56:26 +0800

    Sep_25/

---
+++ Sep_25/test.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+
+class Solution {
+  public:
+    struct ListNode{
+      int val;
+      struct ListNode* next;
+
+    }ListNode;
+
+    ListNode(int n)
+    {}val(n),next(NULL);
+    int LastRemaining_Solution(int n, int m)
+    {
+      ListNode* head = new ListNode(0);
+
+
+
+    }
+
+};
